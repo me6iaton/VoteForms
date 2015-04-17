7066dad80e25c8ef9931c72653219c292b1dfe8f
me6iaton <me6iaton@gmail.com>
2015-04-17 22:12:22 +0300

    add form/create processor

---
+++ core/components/voteforms/processors/mgr/form/create.class.php
@@ -0,0 +1,30 @@
+<?php
+
+/**
+ * Create an Form
+ */
+class VoteFormCreateProcessor extends modObjectCreateProcessor {
+  public $objectType = 'VoteForm';
+  public $classKey = 'VoteForm';
+  public $languageTopics = array('voteforms');
+  //public $permission = 'create';
+
+
+  /**
+   * @return bool
+   */
+  public function beforeSet() {
+    $name = trim($this->getProperty('name'));
+    if (empty($name)) {
+      $this->modx->error->addField('name', $this->modx->lexicon('voteforms_err_name'));
+    }
+    elseif ($this->modx->getCount($this->classKey, array('name' => $name))) {
+      $this->modx->error->addField('name', $this->modx->lexicon('voteforms_err_ae'));
+    }
+
+    return parent::beforeSet();
+  }
+
+}
+
+return 'VoteFormCreateProcessor';
